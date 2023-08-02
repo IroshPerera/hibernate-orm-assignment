a22480ed49c0ac3a62bb92d44a918dec56595187
IroshPerera <iroshlashan31@gmail.com>
2023-08-03 00:21:40 +0530

    add Book class

---
+++ orm_assignment_part_02/one_to_many_relationship/src/main/java/entity/Book.java
@@ -0,0 +1,28 @@
+package entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Book {
+
+    @Id
+    private String book_id;
+    private String book_title;
+    private String book_isbn;
+
+    @ManyToOne
+    private Author author;
+
+
+}
